2b2642d0b82c446f82e8e89f0fffc4e2758765c2
shopik99 <shopiktem@gmail.com>
2026-03-11 23:36:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 import streamlit as st
 import requests
+
+CLIENT_ID = st.secrets["CANVA_CLIENT_ID"]
+CLIENT_SECRET = st.secrets["CANVA_CLIENT_SECRET"]
+REDIRECT_URI = "https://shopikai.streamlit.app/oauth_callback"
+AUTH_CODE = "браузерден келген code"
+
+token_url = "https://api.canva.com/token"
+data = {
+    "grant_type": "authorization_code",
+    "client_id": CLIENT_ID,
+    "client_secret": CLIENT_SECRET,
+    "redirect_uri": REDIRECT_URI,
+    "code": AUTH_CODE
+}
+
+response = requests.post(token_url, data=data)
+access_token = response.json().get("access_token")
+print("Canva Access Token:", access_token)
+import streamlit as st
+import requests
 import json
 
 st.set_page_config(page_title="Canva презентация генератор", page_icon="🎓")
